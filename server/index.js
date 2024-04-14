01e236195256aeed40520bfbf7e7524ecc2840ad
achaltelmasre <achaltelmasre@gmail.com>
2024-04-14 20:35:15 +0530

    create order apis

--- server/index.js
+++ server/index.js
@@ -183,6 +183,133 @@ app.get("/product/:id", async (req, res) => {
     });
   });
 
+  //post /order
+  app.post("/order", async (req, res) => {
+    const {
+        user,
+        product,
+        quantity,
+        shippingAddress,
+        deliveryCharges
+    } = req.body;
+
+    const order = new Order({
+        user: user ,
+        product: product,
+        quantity: quantity,
+        shippingAddress: shippingAddress,
+        deliveryCharges: deliveryCharges
+    });
+
+   try{
+        const savedOrder = await order.save();
+
+        res.json({
+            success: true,
+            data: savedOrder,
+            message: "Order created successfully"
+        });
+    }
+    catch(e){
+        res.json({
+            success: false,
+            message: e.message
+        });
+    }
+ });
+
+ //Get / order/:id
+app.get("/order/:id", async (req, res) =>{
+    const {id} = req.params;
+
+    const order = await Order.findById(id).populate("user product");
+
+    order.user.password = undefined;
+    // order.user.gender = undefined;
+    // order.user.address = undefined;
+    // order.user.createdAt = undefined;
+    // order.user.updatedAt = undefined;
+
+    res.json({
+      success: true,
+      data: order,
+      message: "Order fetched successfully"
+    });
+});
+
+//Get/orders/user/:id
+app.get("/orders/user/:id", async(req, res) =>{
+    const {id} = req.params;
+
+    const orders = await Order.find({user: id}).populate('product user');
+    
+    res.json({
+        success: true,
+        data: orders,
+        message: "Order fetched successfully"
+    });
+});
+
+//Patch/order/status/:id
+app.patch("/order/status/:id", async(req, res) => {
+    const {id} = req.params;
+    const {status} = req.body;
+   
+    const STATUS_PRIORITY_MAP = {
+      pending:0,
+      shipped:1,
+      delivered:2,
+      returned:3,
+      cancelled:4,
+      rejected:5
+    }
+   
+    const order = await Order.findById(id);
+    const currentStatus = order.status;
+   
+    const currentPriority = STATUS_PRIORITY_MAP [currentStatus];
+    const newPriority = STATUS_PRIORITY_MAP [status];
+   
+    if(currentPriority > newPriority) {
+       
+       return res.json({
+           success: false,
+           message: `${status} cannot be set once order is ${currentStatus}`
+       });
+    }
+   
+    await Order.updateOne({_id: id}, {$set: {status: status}});
+   
+   
+   
+    await Order.updateOne ({_id: id},
+       {$set: {status:status}});
+   
+       const updatedProduct = await Order.findOne({_id: id})
+   
+       res.json({
+           success: true,
+           data: updatedProduct,
+           message:" Order status updated successfully"
+       });
+       
+   });
+
+
+//Get /orders
+app.get("/orders", async(req, res) => {
+    const orders = await Order.find();
+  
+    res.json({
+      success: true,
+      data:orders,
+      message: "Orders fetched successfully"
+    });
+  });
+  
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
