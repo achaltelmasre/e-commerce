4cf7ae89c379604cc9a596be3ae69d46b9b1d2ce
achaltelmasre <achaltelmasre@gmail.com>
2024-04-15 19:07:49 +0530

    create navbar & added in home page

---
+++ client/src/views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+import Navbar from "../../component/Navbar/Navbar";
+
+function Home () {
+   return(
+     <>
+     <div>
+        <Navbar/>
+     <h1>Home</h1>
+     </div>
+        
+     </>
+   ) 
+}
+export default Home;
